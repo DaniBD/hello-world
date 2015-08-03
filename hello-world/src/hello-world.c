811e4f3c4a842ad6b06e4c116d96a63a59683cc3
Daniel BD <dani.bd@gmail.com>
2015-08-03 21:32:23 +0200

    First try

---
+++ hello-world/src/hello-world.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : hello-world.c
+ Author      : DaniBD
+ Version     :
+ Copyright   : 
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Hola Mundo"); /* prints Hola Mundo */
+	return EXIT_SUCCESS;
+}
